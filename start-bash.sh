606c97beb9ee102c5044bdd244c6cecace5789d5
Julien9969 <julien.roux404@gmail.com>
2023-07-14 17:30:35 +0200

    auto start v2

--- start-bash.sh
+++ start-bash.sh
@@ -4,3 +4,9 @@ source ${HOME}/.bashrc
 cd /home/trizotto/discord-bot
 # export NODE_ENV=development
 npm run start
+
+
+sudo systemctl stop auto-start.service
+sudo systemctl daemon-reload
+sudo systemctl start auto-start.service
+sudo systemctl status auto-start.service
