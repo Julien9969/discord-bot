7540f60d577fae12c616eb563a8efecfb65baabb
Julien9969 <julien.roux404@gmail.com>
2023-07-14 19:32:31 +0200

    test hooks

--- src/bot.js
+++ src/bot.js
@@ -64,7 +64,7 @@ client.on('interactionCreate', async (interaction) => {
 	const { commandName } = interaction;
 
 	if (commandName === 'hello') {
-		await interaction.reply('test de push sur le serveur');
+		await interaction.reply('Bonjour!');
 	}
 	else if (commandName === 'server') {
 		await interaction.reply('Server info.');
