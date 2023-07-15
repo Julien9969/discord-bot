645303a7397b7c6f4d7aba948f941e711da2548b
Julien9969 <julien.roux404@gmail.com>
2023-07-15 02:40:26 +0200

    okkokok

--- src/bot.js
+++ src/bot.js
@@ -64,7 +64,7 @@ client.on('interactionCreate', async (interaction) => {
 	const { commandName } = interaction;
 
 	if (commandName === 'hello') {
-		await interaction.reply('Bonjour! Je suis BilalBot 34wda dwdawd  wda!');
+		await interaction.reply('Bonjour!');
 	}
 	else if (commandName === 'server') {
 		await interaction.reply('Server info.');
